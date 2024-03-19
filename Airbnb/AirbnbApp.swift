64e4dfda4c121ef4130c956d74b9d811446da26b
akash <akashs@gmail.com>
2024-03-19 13:10:29 +0530

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Shruti Makwana on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
